42481e6c5e85360b411997e8a19a638151f4319f
Daniel Berry <daniel@jstreetdigital.com>
2022-08-12 06:47:45 -0500

    update User model to use UsesUuid trait
    Signed-off-by: Daniel Berry <daniel@jstreetdigital.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\UsesUuid;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -17,6 +18,7 @@ class User extends Authenticatable implements MustVerifyEmail
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use UsesUuid;
 
     /**
      * The attributes that are mass assignable.
