7626cef708dd5e6220a2afc82a15ea8373c95629
Daniel Berry <daniel@jstreetdigital.com>
2022-08-14 17:25:49 -0500

    add user resource and collection resource
    Signed-off-by: Daniel Berry <daniel@jstreetdigital.com>

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'email_verified' => !$this->email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
